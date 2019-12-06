6121095824ebd0b27d8a91de1ff3c312f4f440b6
Thomas Koppensteiner <thomas.koppensteiner@space-4.me>
2019-12-06 21:33:58 +0100

    Move WebExtension::Helper into separate file

--- lib/sidekiq/undertaker.rb
+++ lib/sidekiq/undertaker.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 require "sidekiq/undertaker/version"
-require "sidekiq/undertaker/dead_job"
-require "sidekiq/undertaker/job_filter"
 require "sidekiq/undertaker/web_extension"
-require "sidekiq/undertaker/bucket"
-require "sidekiq/undertaker/job_aggregator"
 
 begin
   require "sidekiq/web"
